d5c0c71bfd145d4f78d253f00dd499571ebdb1dd
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2023-07-23 18:38:07 +0100

    Fix Video aspect ratio

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -4,12 +4,14 @@ interface Props {
 
 export default function Video({ id }: Props) {
   return (
-    <iframe
-      className="w-full"
-      src={`https://www.youtube-nocookie.com/embed/${id}`}
-      title="YouTube video player"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-    />
+    <div className="aspect-video">
+      <iframe
+        src={`https://www.youtube-nocookie.com/embed/${id}`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        className="h-full w-full"
+      />
+    </div>
   );
 }
