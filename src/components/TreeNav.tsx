1b3f95571bbaedba56b12de31f6ca2ebc8c8c2ac
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2023-07-31 08:05:54 +0100

    Split TreeNav into sub components

--- src/components/TreeNav.tsx
+++ src/components/TreeNav.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ModuleCategory } from '@/types';
+import { Module, ModuleCategory } from '@/types';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { MdChevronRight, MdExpandMore } from 'react-icons/md';
@@ -12,43 +12,58 @@ interface Props {
 }
 
 export default function TreeNav({ category, depth = 1 }: Props) {
-  const pathname = usePathname();
-
   return (
     <ul className="list-none text-gray-500">
       {category.sub_categories.map((sub_category) => (
-        <li key={sub_category.slug}>
-          <Link
-            href={sub_category.path}
-            className="relative flex justify-between py-1 transition-colors duration-200 hover:text-gray-950"
-          >
-            {pathname.startsWith(sub_category.path) && depth > 1 && (
-              <div className="absolute -left-[14px] bottom-0 top-0 w-[3px] bg-mia-blue" />
-            )}
-            {sub_category.name}
-            {pathname.startsWith(sub_category.path) ? <MdExpandMore /> : <MdChevronRight />}
-          </Link>
-
-          {pathname.startsWith(sub_category.path) && (
-            <div className="my-2 ml-4 border-l border-gray-200 pl-3">
-              {TreeNav({ category: sub_category, depth: depth + 1 })}
-            </div>
-          )}
-        </li>
+        <InternalNode key={sub_category.slug} category={sub_category} depth={depth} />
       ))}
       {category.modules.map((module) => (
-        <li
-          key={module.slug}
-          className="relative transition-colors duration-200 hover:text-gray-950"
-        >
-          <Link href={module.path} className="flex py-1">
-            {module.name}
-          </Link>
-          {pathname === module.path && (
-            <div className="absolute -left-[14px] bottom-0 top-0 w-[3px] bg-mia-blue" />
-          )}
-        </li>
+        <LeafNode key={module.slug} module={module} />
       ))}
     </ul>
+  );
+}
+
+function InternalNode({ category, depth }: { category: ModuleCategory; depth: number }) {
+  const pathname = usePathname();
+
+  const expanded = pathname.startsWith(category.path);
+
+  // TODO: Add support for collapsing the node
+
+  return (
+    <li>
+      <Link
+        href={category.path}
+        className="relative flex justify-between py-1 transition-colors duration-200 hover:text-gray-950"
+      >
+        {expanded && depth > 1 && (
+          <div className="absolute -left-[14px] bottom-0 top-0 w-[3px] bg-mia-blue" />
+        )}
+        {category.name}
+        {expanded ? <MdExpandMore /> : <MdChevronRight />}
+      </Link>
+
+      {expanded && (
+        <div className="my-2 ml-4 border-l border-gray-200 pl-3">
+          {TreeNav({ category, depth: depth + 1 })}
+        </div>
+      )}
+    </li>
+  );
+}
+
+function LeafNode({ module }: { module: Module }) {
+  const pathname = usePathname();
+
+  return (
+    <li className="relative transition-colors duration-200 hover:text-gray-950">
+      <Link href={module.path} className="flex py-1">
+        {module.name}
+      </Link>
+      {pathname === module.path && (
+        <div className="absolute -left-[14px] bottom-0 top-0 w-[3px] bg-mia-blue" />
+      )}
+    </li>
   );
 }
