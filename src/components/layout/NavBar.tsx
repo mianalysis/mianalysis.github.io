da9591fd9d12b478eb64d5c228b7e88e2bef2e44
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2023-08-16 07:37:46 +0100

    Integrated DocSearch

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -7,6 +7,7 @@ import { MdMenu } from 'react-icons/md';
 import classNames from 'classnames';
 import { useClickOutside } from '@/hooks/click';
 import { delay } from '@/lib/util';
+import Search from '@/components/Search';
 
 const links = (
   <>
@@ -35,21 +36,29 @@ export default function NavBar() {
       <div className="flex justify-between gap-x-8 py-2 max-sm:px-4">
         <Logo />
 
-        <ul className="flex max-sm:hidden list-none items-center gap-x-8">{links}</ul>
+        <ul className="flex max-sm:hidden list-none items-center gap-x-8">
+          {links}
+          <li>
+            <Search />
+          </li>
+        </ul>
 
         <button type="button" onClick={toggle} className="sm:hidden" ref={buttonRef}>
           <MdMenu size={32} />
         </button>
       </div>
 
-      <ul
+      <div
         className={classNames(
-          'list-none drop space-y-2 overflow-hidden transition-all bg-gray-100 p-4 drop-shadow-lg',
-          { 'h-0 py-0': !expanded }
+          'flex justify-between items-start overflow-hidden transition-all bg-gray-100 p-4 drop-shadow-lg',
+          {
+            'h-0 py-0': !expanded,
+          }
         )}
       >
-        {links}
-      </ul>
+        <ul className="list-none space-y-2">{links}</ul>
+        <Search />
+      </div>
     </nav>
   );
 }
