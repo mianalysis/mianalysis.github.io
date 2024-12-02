4c5644841c4b7e1bb04a2c369901ec28475c7b0a
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2024-12-02 21:08:33 +0000

    Patched TreeNav expansion

--- src/components/TreeNav.tsx
+++ src/components/TreeNav.tsx
@@ -36,25 +36,25 @@ function InternalNode({
   currentPath: string;
   depth: number;
 }) {
-  const [hidden, setHidden] = useState(false);
+  const relatesToCurrentPath = currentPath.includes(node.path);
 
-  const toggleHidden = () => setHidden((hidden) => !hidden && currentPath.endsWith(node.path));
+  const [expanded, setExpanded] = useState(relatesToCurrentPath);
 
-  const expanded = currentPath.includes(node.path) && !hidden;
+  const toggleExpanded = () => setExpanded((e) => !e);
 
   return (
     <li>
-      <a
-        href={node.path}
-        className="relative flex justify-between py-1 transition-colors duration-200 hover:text-gray-950"
-        onClick={toggleHidden}
-      >
-        {expanded && depth > 1 && (
+      <div className="relative flex justify-between py-1 transition-colors duration-200 hover:text-gray-950">
+        {relatesToCurrentPath && depth > 1 && (
           <div className="absolute -left-[14px] bottom-0 top-0 w-[3px] bg-mia-blue" />
         )}
-        {node.name}
-        {expanded ? <MdExpandMore /> : <MdChevronRight />}
-      </a>
+
+        <a href={node.path}>{node.name}</a>
+
+        <button type="button" onClick={toggleExpanded} className="p-1">
+          {expanded ? <MdExpandMore /> : <MdChevronRight />}
+        </button>
+      </div>
 
       {expanded && (
         <div className="ml-4 border-l border-gray-200 pl-3 delay-75 my-2">
